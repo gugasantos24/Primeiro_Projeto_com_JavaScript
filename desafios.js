b2aa5c5465d3bc6937ee5a5a607c0867ffb8c7e8
Gustavo <gst.santos01@gmail.com>
2025-04-24 08:26:56 -0300

    Finalizando projeto

--- desafios.js
+++ desafios.js
@@ -1,45 +1,45 @@
-// let nome = 'Gustavo';
-// console.log(`Bem vindo ${nome}`)
-// alert(`Olá ${nome}`);
-// let linguagem = prompt("Qual linguagem de programação você mais gosta? ")
-// console.log(linguagem);
+let nome = 'Gustavo';
+console.log(`Bem vindo ${nome}`)
+alert(`Olá ${nome}`);
+let linguagem = prompt("Qual linguagem de programação você mais gosta? ")
+console.log(linguagem);
 
-// let valor1 = 6;
-// let valor2 = 4;
-// let resultado = valor1 - valor2;
-// console.log(`A diferença entre ${valor1} e ${valor2} é ${resultado}`);
+let valor1 = 6;
+let valor2 = 4;
+let resultado = valor1 - valor2;
+console.log(`A diferença entre ${valor1} e ${valor2} é ${resultado}`);
 
-// let idade = prompt("Diga sua idade ")
-// idade = Number(idade);
+let idade = prompt("Diga sua idade ")
+idade = Number(idade);
 
-// if (idade < 18){
-// console.log("Você é menor de idade");
-// } else {
-// console.log("Você é maior de idade");
-// }
+if (idade < 18){
+console.log("Você é menor de idade");
+} else {
+console.log("Você é maior de idade");
+}
 
-// let numero = prompt("Diga um numero: ");
-// numero = Number(numero);
+let numero1 = prompt("Diga um numero: ");
+numero1 = Number(numero1);
 
-// if (numero >= 1){
-//   console.log("Esse número é positivo");
-// } else if (numero <= -1){
-//   console.log("Esse número é negativo")
-// } else {
-//   console.log("Esse número é zero")
-// }
+if (numero1 >= 1){
+  console.log("Esse número é positivo");
+} else if (numero1 <= -1){
+  console.log("Esse número é negativo")
+} else {
+  console.log("Esse número é zero")
+}
 
-// let numero = 1;
+let numero2 = 1;
 
-// while (numero <= 10){
-//   console.log(numero);
-//   numero++;
-// }
+while (numero2 <= 10){
+  console.log(numero2);
+  numero2++;
+}
 
-// let numero = parseInt(Math.random() * 10 + 1);
+let numero3 = parseInt(Math.random() * 10 + 1);
 
-// console.log(numero);
+console.log(numero3);
 
-// let numero = parseInt(Math.random() * 1000 + 1);
+let numero4 = parseInt(Math.random() * 1000 + 1);
 
-// console.log(numero);
+console.log(numero4);
